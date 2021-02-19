497dc2a4c15c7773705dcdff0ed173711924d8a1
Dj51401's Desktop <dj51401@gmail.com>
2021-02-19 12:18:27 -0700

    Added world

---
+++ WeeklyJam189/Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void OnDisable(){  }
+    void OnEnable(){  }
+    void Awake(){  }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
